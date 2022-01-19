6df93c3eb0f95bf36566826bc902b174bc550838
Anna Bogacheva <annab070703@gmail.com>
2022-01-20 00:01:34 +0300

    api + some changes

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import listener
 import json
+import uvicorn
 
 from fastapi import FastAPI
 
 CRYPTO_API = FastAPI()
+
 
 @CRYPTO_API.get("/")
 def get_events(exchange: str, instrument: str, start_timestamp: int, finish_timestamp: int):
@@ -14,3 +16,7 @@ def get_events(exchange: str, instrument: str, start_timestamp: int, finish_time
         tmp = '[' + i[1] + ']'
         res += json.loads(tmp)
     return json.dumps(res)
+
+
+if __name__ == "__main__":
+    uvicorn.run(CRYPTO_API, host="0.0.0.0", port=8000)
