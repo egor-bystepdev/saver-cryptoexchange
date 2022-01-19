406eff67a6596c1e2dd7ab12d793ca6a7a5544dc
Anna Bogacheva <annab070703@gmail.com>
2022-01-19 20:54:31 +0300

    api + some changes

---
+++ main.py
@@ -0,0 +1,15 @@
+import listener
+import json
+
+
+from fastapi import FastAPI
+app = FastAPI()
+@app.get("/")
+def get_events(exchange : str, instrument : str, start_timestamp : int, finish_timestamp : int):
+    ans = listener.get_all_msg_in_db(exchange, instrument, start_timestamp,
+                                     finish_timestamp)  # хочу понять, надо ли как-то изменять ответный джейсон, изменять поля или разделять по data_type
+    res = []
+    for i in ans:
+        tmp = '[' + i[2] + ']'
+        res += json.loads(tmp)
+    return json.dumps(res)
