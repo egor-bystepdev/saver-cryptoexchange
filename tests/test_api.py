b2750a653e414e66d6211304d58a1c4dc8a6f6d9
Fedor Osetrov <fiosetrova@gmail.com>
2022-01-22 01:36:26 +0300

    Add test for bad request

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,16 @@
+from curses.ascii import HT
 import requests
+from http import HTTPStatus
 
 
 def test_check_status_code_equals_200():
     response = requests.get("http://127.0.0.1:8000/",
-                            params={'exchange': "binance", 'instrument': "BNBBTC", 'start_timestamp': 1642604400000,
-                                    'finish_timestamp': 1642615078957})
-    assert response.status_code == 200
+                            params={'exchange': "binance", 'instrument': "BNBBTC", 'start_timestamp': 0,
+                                    'finish_timestamp': 0})
+    assert response.status_code == HTTPStatus.OK
+
+def test_check_status_code_equals_422():
+    response = requests.get("http://127.0.0.1:8000/",
+                            params={'exchange': "binance", 'instrument': "BNBBTC", 'start_timestamp': 1642604400000})
+
+    assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
