650cef10d16af2c53dc497b1f320eee9605926e2
Fedor Osetrov <fiosetrova@gmail.com>
2022-05-10 19:45:54 +0300

    Add support of start for several symbols and their stop

--- listener/websocketsftx/threaded_websocket_manager.py
+++ listener/websocketsftx/threaded_websocket_manager.py
@@ -1,38 +1,66 @@
 import threading
 import time
 from websocketsftx.client import FtxWebsocketClient
+from utils.helpers import create_logger
     
 class FTXThreadedWebsocketManager:
-    def __init__(self, data_types : list, sleep_time, symbol : str, api_key : str, api_secret : str):
-        self.data_types = data_types
+    def __init__(self, api_key : str, api_secret : str, number=1, sleep_time=1):
+        self.data_types = ["trades", "orderbook"]
         self.sleep_time = sleep_time
-        self.symbol = symbol
         self.api_key = api_key
         self.api_secret = api_secret
-    
-    def start(self, callback):
-        self.threads = []
-        for data_type in self.data_types:
-            self.threads.append(threading.Thread(target=create_thread_for_stream,
-                                                args=(self.symbol, data_type, callback, self.sleep_time, self.api_key, self.api_secret)))
 
-        for thread in self.threads:
+        self.threads = {}
+        self.clients = {}
+
+        self.log = create_logger(f"FTX TWM ({number})")
+    
+    def start(self, callback, symbol):
+        self.threads[symbol] = []
+        self.clients[symbol] = {
+            "trades": FtxWebsocketClient(self.api_key, self.api_secret),
+            "orderbook": FtxWebsocketClient(self.api_key, self.api_secret),
+        }
+
+        for data_type in self.data_types:
+            self.threads[symbol].append(threading.Thread(target=create_thread_for_stream,
+                                                args=(self.clients[symbol], symbol, data_type, callback, self.sleep_time)))
+
+        type = 0
+        for thread in self.threads[symbol]:
+            self.log.info(f"start listening data type ({self.data_types[type]}) for symbol ({symbol})\n")
             thread.start()
+            type += 1
     
     def join(self):
-        for thread in self.threads:
-            thread.join()
+        for symbol in self.threads:
+            for data_type in self.data_types:
+                self.clients[symbol][data_type].unsubscribe(symbol, data_type)
 
-    def stop(self):
-        for thread in self.threads:
-            thread.join()
+            for thread in self.threads[symbol]:
+                thread.join()
 
-def create_thread_for_stream(symbol, data_type, callback, sleep_time, api_key, api_secret):
-    client = FtxWebsocketClient(api_key, api_secret)
+    def stop(self, symbol):
+        if symbol not in self.threads:
+            self.log.info(f"symbol ({symbol}) is not listened at this moment")
+            return
+
+        for data_type in self.data_types:
+            self.clients[symbol][data_type].unsubscribe(symbol, data_type)
+        del self.clients[symbol]
+
+        for thread in self.threads[symbol]:
+            thread.join()
+        del self.threads[symbol]
+
+def create_thread_for_stream(clients, symbol, data_type, callback, sleep_time):
     while True:
+        if clients[data_type].stopped():
+            break
+
         if data_type == "trades":
-            callback(client.get_trades(symbol), data_type)
+            callback(clients[data_type].get_trades(symbol), data_type)
         elif data_type == "orderbook":
-            callback(client.get_orderbook(symbol), data_type)
+            callback(clients[data_type].get_orderbook(symbol), data_type)
 
         time.sleep(sleep_time)
