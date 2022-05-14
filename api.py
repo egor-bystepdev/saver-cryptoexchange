4d20f87058b1caaa151005680256a27db0bf18dc
Bogacheva Anna <aabogacheva@yandex-team.ru>
2022-05-14 16:15:55 +0300

    api

--- api.py
+++ api.py
@@ -1,8 +1,25 @@
-import listener
 import json
+import time
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi import HTTPException
+from fastapi.responses import JSONResponse
+from prometheus_client import start_http_server, Counter, Histogram
+from prometheus_fastapi_instrumentator import Instrumentator
+
+import listener
+
+graphs = {}
+graphs["counter"] = Counter(
+    "app_http_request_count",
+    "The Total number of HTTP Application request"
+)
+graphs["histogram"] = Histogram(
+    "app_http_response_time",
+    "The time of HTTP Application response",
+    buckets=(1, 2, 5, 6, 10, float("inf"))  # Positive Infinity
+)
 
 exchange_data_types = {
     "binance": ["trade", "kline", "depthUpdate"],
@@ -10,12 +27,21 @@ exchange_data_types = {
 }
 
 exchanges = {"binance", "ftx"}
-
 CRYPTO_API = FastAPI()
+instrumentator = Instrumentator(
+    should_ignore_untemplated=True,
+    should_instrument_requests_inprogress=True,
+    excluded_handlers=["/metrics"],
+    inprogress_name="inprogress",
+    inprogress_labels=True,
+)
+instrumentator.instrument(CRYPTO_API).expose(CRYPTO_API)
 
 
 @CRYPTO_API.get("/")
 def get_events(exchange: str, instrument: str, start_timestamp: int, finish_timestamp: int):
+    graphs["counter"].inc()
+    start_time = time.time()
     if exchange not in exchanges:
         log_text = f"not available exchange {exchange}"
         listener.handle_error("get_events api method", log_text, listener_db.logger)
@@ -29,7 +55,9 @@ def get_events(exchange: str, instrument: str, start_timestamp: int, finish_time
     for event in events:
         tmp = '[' + event[1] + ']'
         res += json.loads(tmp)
-
+    end_time = time.time()
+    graphs["histogram"].observe(end_time - start_time)
+    print(graphs)
     return json.dumps(res)
 
 
@@ -40,6 +68,8 @@ def stop(exchange: str, instrument: str):
 
 @CRYPTO_API.get("/start")
 def start(exchange: str, instrument: str):
+    graphs["counter"].inc()
+    start_time = time.time()
     if exchange not in exchanges:
         log_text = f"not available exchange {exchange}"
         listener.handle_error("get_events api method", log_text, listener_db.logger)
@@ -48,8 +78,17 @@ def start(exchange: str, instrument: str):
     if not started:
         listener.handle_error("start api method", log_text, listener_db.logger)
         raise HTTPException(status_code=404, detail=log_text)
+    end_time = time.time()
+    graphs["histogram"].observe(end_time - start_time)
+    print(graphs)
+    return JSONResponse(status_code=status.HTTP_200_OK, content="DB started")
+
+
+@CRYPTO_API.on_event("startup")
+def startup_event():
+    start_http_server(port=9090)
 
 
 if __name__ == "__main__":
     listener_db = listener.ListenerManager()
-    uvicorn.run(CRYPTO_API, host="0.0.0.0", port=8000)
+    uvicorn.run(CRYPTO_API, host="0.0.0.0", port=8080)
--- tests/test_api.py
+++ tests/test_api.py
@@ -4,13 +4,13 @@ from http import HTTPStatus
 
 
 def test_check_status_code_equals_200():
-    response = requests.get("http://127.0.0.1:8000/",
+    response = requests.get("http://127.0.0.1:8080/",
                             params={'exchange': "binance", 'instrument': "BNBBTC", 'start_timestamp': 0,
                                     'finish_timestamp': 0})
     assert response.status_code == HTTPStatus.OK
 
 
 def test_check_status_code_equals_422():
-    response = requests.get("http://127.0.0.1:8000/",
+    response = requests.get("http://127.0.0.1:8080/",
                             params={'exchange': "binance", 'instrument': "BNBBTC", 'start_timestamp': 1642604400000})
     assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
